fbdf2a09a492e1090863c7f285a5c456fc67cc1d
Ashok Kumar <akumar@unitedlayer.com>
2023-05-23 22:06:42 +0530

    added font awesome to flocit home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { AppComponent } from './app.component';
 import { InvalidRouteComponent } from './invalid-route/invalid-route.component';
 import { UnderMaintenanceComponent } from './under-maintenance/under-maintenance.component';
 import { HomeComponent } from './home/home.component';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 @NgModule({
   declarations: [
@@ -22,9 +26,14 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private library: FaIconLibrary) {
+    library.addIconPacks(fas, far, fab);
+  }
+}
